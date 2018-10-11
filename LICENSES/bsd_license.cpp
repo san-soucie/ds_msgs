ddd5e7de60ed20d637621b5a5b460abdbc4712a7
Laura Lindzey <llindzey@whoi.edu>
2018-10-11 18:50:42 -0400

    Adding license headers to C++ files

--- LICENSES/bsd_license.cpp
+++ LICENSES/bsd_license.cpp
@@ -27,32 +27,3 @@
 * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 * POSSIBILITY OF SUCH DAMAGE.
 */
-
-#include "ds_nmea_parsers/PixseStatus.h"
-
-namespace ds_nmea_msgs
-{
-
-bool from_string(PixseStatus& output, const std::string &nmea_string)
-{
-  uint32_t lsb;
-  uint32_t msb;
-  const auto n = sscanf(
-      nmea_string.c_str(), "$PIXSE,STATUS,%x,%x*%02hhx\r\n",
-      &lsb, &msb, &output.checksum);
-
-  if (n < 2) {
-    return false;
-  }
-
-  lsb = le32toh(lsb);
-  msb = le32toh(msb);
-
-  output.status = (static_cast<uint64_t>(msb) << 32) + lsb;
-
-  return true;
-}
-
-
-
-}
